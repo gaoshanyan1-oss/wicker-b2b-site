0ff46885d9a4e733895fa258333cd42a58081ca6
高山雁 <gaoshanyan1@gmail.com>
2026-07-10 00:19:12 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const repoName = "wicker-b2b-site";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
